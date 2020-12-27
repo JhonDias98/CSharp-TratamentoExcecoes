2132bd131bc6fe13d479ae6368d0e3b9561b4645
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-12-27 02:48:39 -0300

    Usando Dispose.

--- ByteBank-Excecoes/Program.cs
+++ ByteBank-Excecoes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,53 @@ namespace ByteBank_Excecoes
         {
             try
             {
+                CarregarContas();
+            }
+            catch
+            {
+                Console.WriteLine("Catch no método MAIN");
+            }
+            
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+            using(LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+            //----------------------------------------------
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contas.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //finally
+            //{
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }     
+            //}
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
                 ContaCorrente conta1 = new ContaCorrente(6987, 213687);
                 ContaCorrente conta2 = new ContaCorrente(3287, 634787);
 
                 //conta1.Transferir(2000, conta2);
                 conta1.Sacar(2000);
             }
-            catch(OperacaoFinanceiraException e)
+            catch (OperacaoFinanceiraException e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
@@ -27,21 +68,25 @@ namespace ByteBank_Excecoes
                 Console.WriteLine(e.InnerException.Message);
                 Console.WriteLine(e.InnerException.StackTrace);
             }
-            /*try
+        }
+
+        private static void TestaException()
+        {
+            try
             {
                 ContaCorrente conta2 = new ContaCorrente(146, 695432);
 
                 ContaCorrente conta = new ContaCorrente(146, 326941);
-                
+
                 conta.Transferir(1000, conta2);
             }
-            catch(ArgumentException ex)
+            catch (ArgumentException ex)
             {
                 Console.WriteLine("Argumento com problema: " + ex.ParamName);
                 Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
                 Console.WriteLine(ex.Message);
             }
-            catch(SaldoInsuficienteException ex)
+            catch (SaldoInsuficienteException ex)
             {
                 Console.WriteLine(ex.Saldo);
                 Console.WriteLine(ex.ValorSaque);
@@ -51,11 +96,10 @@ namespace ByteBank_Excecoes
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("Exceção do tipo SaldoInsuficienteException");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-            }*/
-            Console.ReadLine();
+            }
         }
     }
 }
